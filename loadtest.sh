68d3e9fc9639273041a9dc603de936224b0a6cce
hakkiogretmen <hakkiogretmen@gmail.com>
2019-05-16 21:48:03 +0300

    convert to get

--- loadtest.sh
+++ loadtest.sh
@@ -4,49 +4,47 @@ echo "**************************************************************************
 echo "*---------------------------------BEGIN LOAD TEST-------------------------------------*"
 echo "***************************************************************************************"
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
-  echo "Neither SERVICE_IP nor sleep time was passed as a parameter, assuming it is passed as environment variable"
+  echo "APPLICATION_URL was not passed as a parameter, assuming it is passed as environment variable"
 else
-  echo "SERVICE_IP was passed as a parameter"
-  export SERVICE_IP=$1
-  export SLEEP_TIME=$2
+  echo "APPLICATION_URL was passed as a parameter"
+  export APPLICATION_URL=$1
 fi
 
-export CONTENT_TYPE="Content-Type: application/json"
-export PAYLOAD='{"EmailAddress": "email@domain.com", "Product": "prod-1", "Total": 100}'
-export ENDPOINT=http://$SERVICE_IP
+#export CONTENT_TYPE="Content-Type: application/json"
+#export PAYLOAD='{"EmailAddress": "email@domain.com", "Product": "prod-1", "Total": 100}'
+export ENDPOINT=$APPLICATION_URL
 
-echo "POST $ENDPOINT"
-echo $CONTENT_TYPE
-echo $PAYLOAD
+echo "GET $ENDPOINT"
+
 
 echo "Phase 1: Warming up - 30 seconds, 100 users."
-./hey -z 30s -c 100 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
+./hey -z 30s -c 100 "$ENDPOINT"
 
-echo "Waiting 15 seconds for the cluster to stabilize"
-sleep $SLEEP_TIME
+echo "Waiting 5 seconds for the cluster to stabilize"
+sleep 5
 
 echo "\nPhase 2: Load test - 30 seconds, 400 users."
-./hey -z 30s -c 400 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
+./hey -z 30s -c 400 "$ENDPOINT"
 
-echo "Waiting 15 seconds for the cluster to stabilize"
-sleep $SLEEP_TIME
+echo "Waiting 5 seconds for the cluster to stabilize"
+sleep 5
 
 echo "\nPhase 3: Load test - 30 seconds, 1600 users."
-./hey -z 30s -c 1600 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
+./hey -z 30s -c 1600 "$ENDPOINT"
 
-echo "Waiting 15 seconds for the cluster to stabilize"
-sleep $SLEEP_TIME
+echo "Waiting 5 seconds for the cluster to stabilize"
+sleep 5
 
 echo "\nPhase 4: Load test - 30 seconds, 3200 users."
-./hey -z 30s -c 3200 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
+./hey -z 30s -c 3200 "$ENDPOINT"
 
 echo "Waiting 15 seconds for the cluster to stabilize"
-sleep $SLEEP_TIME
+sleep 5
 
 echo "\nPhase 5: Load test - 30 seconds, 6400 users."
-./hey -z 30s -c 6400 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
+./hey -z 30s -c 6400 "$ENDPOINT"
 
 echo "***************************************************************************************"
 echo "*----------------------------------END LOAD TEST--------------------------------------*"
