1b9af154d32d357749179e07d2ff1016b1d5b5a3
hakkiogretmen <32032226+hakkiogretmen@users.noreply.github.com>
2019-05-16 21:33:14 +0300

    Update loadtest.sh

--- loadtest.sh
+++ loadtest.sh
@@ -4,17 +4,18 @@ echo "**************************************************************************
 echo "*---------------------------------BEGIN LOAD TEST-------------------------------------*"
 echo "***************************************************************************************"
 
-if [ -z "$1" ]
+if [ -z "$2" ]
 then
-  echo "No SERVICE_IP was passed as a parameter, assuming it is passed as environment variable"
+  echo "Neither SERVICE_IP nor sleep time was passed as a parameter, assuming it is passed as environment variable"
 else
   echo "SERVICE_IP was passed as a parameter"
   export SERVICE_IP=$1
+  export SLEEP_TIME=$2
 fi
 
 export CONTENT_TYPE="Content-Type: application/json"
 export PAYLOAD='{"EmailAddress": "email@domain.com", "Product": "prod-1", "Total": 100}'
-export ENDPOINT=http://$SERVICE_IP/v1/order
+export ENDPOINT=http://$SERVICE_IP
 
 echo "POST $ENDPOINT"
 echo $CONTENT_TYPE
@@ -24,25 +25,25 @@ echo "Phase 1: Warming up - 30 seconds, 100 users."
 ./hey -z 30s -c 100 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
 
 echo "Waiting 15 seconds for the cluster to stabilize"
-sleep 15
+sleep $SLEEP_TIME
 
 echo "\nPhase 2: Load test - 30 seconds, 400 users."
 ./hey -z 30s -c 400 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
 
 echo "Waiting 15 seconds for the cluster to stabilize"
-sleep 15
+sleep $SLEEP_TIME
 
 echo "\nPhase 3: Load test - 30 seconds, 1600 users."
 ./hey -z 30s -c 1600 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
 
 echo "Waiting 15 seconds for the cluster to stabilize"
-sleep 15
+sleep $SLEEP_TIME
 
 echo "\nPhase 4: Load test - 30 seconds, 3200 users."
 ./hey -z 30s -c 3200 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
 
 echo "Waiting 15 seconds for the cluster to stabilize"
-sleep 15
+sleep $SLEEP_TIME
 
 echo "\nPhase 5: Load test - 30 seconds, 6400 users."
 ./hey -z 30s -c 6400 -d "$PAYLOAD" -H "$CONTENT_TYPE" -m POST "$ENDPOINT"
