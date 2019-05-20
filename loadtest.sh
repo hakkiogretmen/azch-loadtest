7ea8ff881fceb860e433875a0439eaacf1a37aa6
hakkiogretmen <hakkiogretmen@gmail.com>
2019-05-20 20:26:00 +0300

    added parameters

--- loadtest.sh
+++ loadtest.sh
@@ -4,47 +4,44 @@ echo "**************************************************************************
 echo "*---------------------------------BEGIN LOAD TEST-------------------------------------*"
 echo "***************************************************************************************"
 
-if [ -z "$1" ]
+if [ -z "$3" ]
 then
-  echo "APPLICATION_URL was not passed as a parameter, assuming it is passed as environment variable"
+  echo "Some parameters were not passed as a parameter, please refer to help document"
+  exit -1
 else
-  echo "APPLICATION_URL was passed as a parameter"
+  echo "APPLICATION_URL, TEST_DURATION and CONCURRENT_USER  were passed as a parameter"
   export APPLICATION_URL=$1
+  export TEST_DURATION=$2
+  export CONCURRENT_USER=$3
 fi
 
-#export CONTENT_TYPE="Content-Type: application/json"
-#export PAYLOAD='{"EmailAddress": "email@domain.com", "Product": "prod-1", "Total": 100}'
 export ENDPOINT=$APPLICATION_URL
 
-echo "GET $ENDPOINT"
+echo "Application URL: $ENDPOINT"
+echo "Test Duration: $TEST_DURATION"
+echo "Concurrent User Count: $CONCURRENT_USER"
 
 
 echo "Phase 1: Warming up - 30 seconds, 100 users."
 ./hey -z 30s -c 100 "$ENDPOINT"
 
-echo "Waiting 5 seconds for the cluster to stabilize"
-sleep 5
+echo "Waiting 1 seconds for the cluster to stabilize"
+sleep 1
 
 echo "\nPhase 2: Load test - 30 seconds, 400 users."
 ./hey -z 30s -c 400 "$ENDPOINT"
 
-echo "Waiting 5 seconds for the cluster to stabilize"
-sleep 5
+echo "Waiting 1 seconds for the cluster to stabilize"
+sleep 1
 
 echo "\nPhase 3: Load test - 30 seconds, 1600 users."
 ./hey -z 30s -c 1600 "$ENDPOINT"
 
-echo "Waiting 5 seconds for the cluster to stabilize"
-sleep 5
+echo "Waiting 1 seconds for the cluster to stabilize"
+sleep 1
 
-echo "\nPhase 4: Load test - 30 seconds, 3200 users."
-./hey -z 30s -c 3200 "$ENDPOINT"
-
-echo "Waiting 15 seconds for the cluster to stabilize"
-sleep 5
-
-echo "\nPhase 5: Load test - 30 seconds, 6400 users."
-./hey -z 30s -c 6400 "$ENDPOINT"
+echo "\nLoad Test Phase - $TEST_DURATION minutes, $CONCURRENT_USER users."
+./hey -z "${TEST_DURATION}m" -c $CONCURRENT_USER "$ENDPOINT"
 
 echo "***************************************************************************************"
 echo "*----------------------------------END LOAD TEST--------------------------------------*"
